61a1060bbca32817a3474b71b14a22e3b16e9c79
binjaraval <bvs.raval30@gmail.com>
2023-02-19 21:30:53 +0000

    webdriver

---
+++ src/main/java/webdriver/Firefox.java
@@ -0,0 +1,13 @@
+package webdriver;
+
+public class Firefox {
+
+    public static void main(String[] args) throws InterruptedException {
+  //      2. Set up the firefoxdriver - Path
+      System.setProperty("webdriver.chrome.driver","/Users/binjalraval/Downloads/Firefoxdriver/110.0dmg.");
+  //      1. Create an instance of webdriver.
+        //      WebDriver driver = new FirefoxDriver();
+  //        3. Navigate to Webpage
+        //      driver.get("https://demo.nopcommerce.com/");
+        }
+}
---
+++ src/main/java/webdriver/Opera.java
@@ -0,0 +1,13 @@
+package webdriver;
+
+public class Opera {
+    public static void main(String[] args) throws InterruptedException{
+//        2. Set up the operadriver - Path
+        System.setProperty("webdriver.opera.driver","/Users/binjalraval/Downloads/driversOperaSetup (1).");
+ //       1. Create an instance of webdriver
+      //  WebDriver driver = new OperaDriver();
+ //       3. Navigate to Webpage
+      //  driver.get("https://demo.nopcommerce.com/");
+
+    }
+}
